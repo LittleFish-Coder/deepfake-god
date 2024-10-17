f61cbf2ce735ea2789ec087b3d1edc50f3bcf790
LittleFish-Coder <adrianyu890317@gmail.com>
2024-10-17 16:20:48 +0800

    finsh moving

--- dataset/WildDeepfake/preprocess.py
+++ dataset/WildDeepfake/preprocess.py
@@ -29,12 +29,8 @@ if __name__ == '__main__':
 
     for input_dir, output_dir in zip(input_dirs, output_dirs):
         for filename in os.listdir(input_dir):
-            continue
             if filename.endswith(".tar.gz"):
                 input_file = os.path.join(input_dir, filename)
-                # output_dir_name = os.path.splitext(os.path.splitext(filename)[0])[0]
-                # final_output_dir = os.path.join(output_dir, output_dir_name)
-                # os.makedirs(final_output_dir, exist_ok=True)
                 extract_tar_gz(input_file, input_dir)
 
         # move all files from the input folder to the output folder
